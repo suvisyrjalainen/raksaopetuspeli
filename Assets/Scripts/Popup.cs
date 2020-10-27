416c0e9f8b2d0ad8b106b079f2581cc6075b69a7
Kodarit4 <you@example.com>
2020-10-27 15:10:34 +0200

    KysymysUI

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+
+public class MenuOnClick : MonoBehaviour
+{
+
+    public Canvas Quiz_canvas;//Its your Canvas
+
+    void OnMouseDown()
+    {
+        Debug.Log("Pressed left click.");
+        Quiz_canvas.gameObject.SetActive(true);
+        
+    }
+}
