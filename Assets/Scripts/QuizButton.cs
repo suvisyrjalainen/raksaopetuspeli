c6b520197739c01f703d001dc9b5b8af8033e59c
Kodarit4 <you@example.com>
2020-10-28 15:42:27 +0200

    Ei oikein mitään

--- Assets/Scripts/QuizButton.cs
+++ Assets/Scripts/QuizButton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Popup : MonoBehaviour
+public class QuizButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
