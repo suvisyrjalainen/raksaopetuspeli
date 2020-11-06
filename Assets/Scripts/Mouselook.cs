6486dad4dd7a5a7cd3fb17ffd291d5df1d38f286
Kodarit4 <you@example.com>
2020-11-06 15:31:48 +0200

    kartan juttuja korjattu?

--- Assets/Scripts/Mouselook.cs
+++ Assets/Scripts/Mouselook.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+
 
 public class Mouselook : MonoBehaviour
 {
@@ -13,15 +17,20 @@ public class Mouselook : MonoBehaviour
     public float mouseX;
     public float mouseY;
     private float xRotation = 0f;
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
+    int xPos = 0, yPos = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;  
+        SetCursorPos(xPos, yPos);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
 
@@ -30,5 +39,6 @@ public class Mouselook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+        SetCursorPos(xPos, yPos);
     }
 }
